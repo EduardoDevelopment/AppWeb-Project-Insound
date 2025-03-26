96dd9820e50258dbbb4bb1626662032afca3ada6
EduardoDevelopment <lalokeras677@gmail.com>
2025-03-25 22:16:07 -0600

    nuevos cambios

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a9259a38e0944f4ab4e0fbd3478ce56b4a5364ef',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a9259a38e0944f4ab4e0fbd3478ce56b4a5364ef',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '2.1.8',
+            'version' => '2.1.8.0',
+            'reference' => 'f9adff3b87c03b12cc7e46a30a524648e497758f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
